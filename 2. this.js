5c4de7ae6934136d3eaf0a98334c2b336c825f55
Shikhar Dubey <shikhar.db@gmail.com>
2018-12-28 01:05:16 +0530

    Feat : 'this' keyword

---
+++ 2. this.js
@@ -0,0 +1,23 @@
+/* 4 rules for how the 'this' keyword gets bound
+ *
+ * 1.
+ * 2.
+ * 3. Implicit Binding - 'this' will point to the object from which it is called
+ * 4. Default binding - in strict mode : undefined, in non-strict mode : global
+ *
+ * It all depends on the call site.
+ */
+
+function foo() {
+  console.log(this.bar);
+}
+
+var bar = 'bar1';
+var o2 = { bar: 'bar2', foo: foo }; // here we have 2 references -  o2.foo and global foo()
+var o3 = { bar: 'bar3', foo: foo };
+
+foo(); // Default binding
+o2.foo();
+o3.foo();
+
+// if IIFE, it's gonna use the Default Rule
